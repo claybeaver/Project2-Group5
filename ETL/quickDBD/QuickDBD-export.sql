4d80ffd4d96692fc786f3b0cdecd5d18dc8bb689
Irina Kim <irinahpda@gmail.com>
2020-09-04 22:03:28 +0100

    Web scrape fatalities. Adjust QuickDB postgreSQL query file. Push 3 tables to DB - hurricanes, cost, fatalities.

--- ETL/quickDBD/QuickDBD-export.sql
+++ ETL/quickDBD/QuickDBD-export.sql
@@ -1,16 +1,44 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 -- Modify this code to update the DB schema diagram.
 -- To reset the sample schema, replace everything with
 -- two dots ('..' - without quotes).
 
-CREATE TABLE "Cost" (
+CREATE TABLE "cost" (
     "hurricane_id" INT   NOT NULL,
     "name" VARCHAR(255)   NOT NULL,
-    "damage_usd" INT   NOT NULL,
-    "norm_damage_usd" INT   NOT NULL,
-    "year" INT   NOT NULL,
-    "category" VARCHAR(255)   NOT NULL
+    "damage_usd" numeric   NOT NULL,
+    "norm_damage_usd" numeric   NOT NULL,
+    "year" INT   NOT NULL
 );
 
+CREATE TABLE "fatalities" (
+    "hurricane_id" INT   NOT NULL,
+    "name" VARCHAR(255)   NOT NULL,
+    "year" INT   NOT NULL,
+    "deaths" INT   NOT NULL
+);
+
+CREATE TABLE "hurricanes" (
+    "hurricane_id" INT   NOT NULL,
+    "date" INT   NOT NULL,
+    "name" VARCHAR(255)   NOT NULL,
+    "time" INT   NOT NULL,
+    "status" VARCHAR(255)   NOT NULL,
+    "latitude" VARCHAR(255)   NOT NULL,
+    "longitude" VARCHAR(255)   NOT NULL,
+    "max_wind" INT   NOT NULL,
+    "air_pressure" INT   NOT NULL,
+    "latitude_decimal" numeric   NOT NULL,
+    "longitude_decimal" numeric   NOT NULL,
+    "year" INT   NOT NULL
+);
+
+select * from hurricanes;
+select * from cost;
+select * from fatalities;
+
+drop table hurricanes;
+drop table cost;
+drop table fatalities;
