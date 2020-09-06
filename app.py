02862dbc128784d24993aa973131a212098e87cd
Irina Kim <irinahpda@gmail.com>
2020-09-06 23:10:59 +0100

    Replace negative values in wind speed with zeroes for plotly.

--- app.py
+++ app.py
@@ -70,34 +70,14 @@ def jsondata():
     j = json.dumps(objects_list)
     objects_file = 'master_objects.js'
     f = open(objects_file,'w')
-    print(f, j)
+    # print(f, j)
     # return render_template("jsondata.html", data=j)    
     return j
 
 # create route that renders index.html template
 @app.route("/")
 def home():
-    cursor.execute("select name, hurricane_id, year, latitude_decimal, longitude_decimal, max_wind, air_pressure, time from master")
-    rows = cursor.fetchall()
-    objects_list = []
-    for row in rows:
-        d = collections.OrderedDict()
-        d['name'] = row[0]
-        d['hurricane_id'] = row[1]
-        d['year'] = row[2]
-        d['latitude'] = str(row[3])
-        d['longitude'] = str(row[4])
-        d['max_wind'] = row[5]
-        d['air_pressure'] = row[6]
-        d['time'] = row[7]
-        objects_list.append(d)
-
-    j = json.dumps(objects_list)
-    objects_file = 'master_objects.js'
-    f = open(objects_file,'w')
-    # print(f, j)
-    return render_template("index.html", data=j)
-    # return j
+    return render_template("index.html")
   
 # create route that renders index.html template
 @app.route("/data")
--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,58 @@
+function buildGraph(sample) {
+  const url = "/jsondata";
+  d3.json(url).then(function(data){
+    console.log(data);
+    const names = data.map(entry => entry.name);
+    const maxwind = data.map(entry => entry.max_wind);
+    console.log(names);
+    console.log(maxwind);    
+
+    console.log(Math.max(maxwind));
+    
+    // data.forEach((entry, i) => {
+    //   console.log(entry.name);
+    // })
+
+    const title = `Maximum winds`;
+    const trace = {
+      x: names,
+      y: maxwind,
+      type: 'bar',
+      orientation: 'h',
+      title: title,
+      text: names,
+    };
+    var data = [trace];
+    var layout = {
+      title: {
+        text: title,
+        font: {
+          size: 12
+        },
+      }, 
+      font: {
+        size: 8,
+      },
+      xaxis: { title: "Maximum winds" },
+      yaxis: maxwind,
+      width: 400,
+      margin: {
+        l: 100,
+        r: 10,
+        b: 100,
+        t: 100,
+        pad: 10}
+    };
+    Plotly.newPlot("plot", data, layout);
+  })
+  // console.log("--- Graph built ---");
+};
+
+buildGraph();
+
 function buildPlot() {
   /* data route */
-  const url = "/";
+  const url = "/jsondata";
   d3.json(url).then(function(response) {
 
     console.log(response);
@@ -31,4 +83,4 @@ function buildPlot() {
   });
 }
 
-buildPlot();
+// buildPlot();
